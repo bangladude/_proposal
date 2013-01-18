028729639b4e650472aeb63b41a0e8beda3c9161
Shaanan Cohney <shaananc@gmail.com>
2013-01-18 03:24:14 -0500

    debug enabled

--- php/test/index.php
+++ php/test/index.php
@@ -8,7 +8,7 @@ require_once('__init__.php');
 // This would be the url of the host running the server-standalone.jar
 $wd_host = 'http://ftp.shaanan.cohney.info:4444/wd/hub'; // this is the default
 echo "hello";
-$web_driver = new WebDriver();
+$web_driver = new WebDriver($wd_host);
 
 
 // First param to session() is the 'browserName' (default = 'firefox')
