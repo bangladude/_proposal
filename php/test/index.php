9f867d096cad6c70a2b4d030bc1460a4f41206ba
Shaanan Cohney <shaananc@gmail.com>
2013-01-18 06:03:44 -0500

    trying to print

--- php/test/index.php
+++ php/test/index.php
@@ -29,7 +29,7 @@ if ($user_id) {
     echo $time();
     
     $event_param = array('name' => "Event Name",
-        'start_time' => time() + 60 * 60,
+        'start_time' => '2013-07-04',
         //'end_time' => time() + 60 * 60 * 2,
         //'location' => "Event Location",
         //'description' => "Description",
