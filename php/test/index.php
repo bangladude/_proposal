2475e9c38c1adca57d430e8aef07bee112882120
Shaanan Cohney <shaananc@gmail.com>
2013-01-18 04:09:09 -0500

    starting serious stuff

--- php/test/index.php
+++ php/test/index.php
@@ -1,18 +1,48 @@
-TEST
 <?php
 
-ini_set('display_errors','On'); ini_set('error_reporting','E_ALL | E_STRICT'); error_reporting(E_ALL);
+// Displays errors
+ini_set('display_errors', 'On');
+ini_set('error_reporting', 'E_ALL | E_STRICT');
+error_reporting(E_ALL);
 
-echo "hello";
+function get_facebook_cookie($app_id, $application_secret) {
+    $args = array();
+    parse_str(trim($_COOKIE['fbs_' . $app_id], '"'), $args);
+    ksort($args);
+    $payload = '';
+    foreach ($args as $key => $value) {
+        if ($key != 'sig') {
+            $payload .= $key . '=' . $value;
+            // echo $key.'<br>';
+        }
+    }
+    if (md5($payload . $application_secret) != $args['sig']) {
+        return null;
+    }
+    return $args;
+}
 
-require_once 'goutte.phar';
-use Goutte\Client;
-$client = new Client();
+?>
+<?php
 
-$crawler = $client->request('GET', 'http://www.symfony-project.org/');
-echo $crawler->filter('body')->text();
+//START LOGIN BLOCK
 
+
+session_start();
+$loggedin = 0;
+require_once('fb/facebook.php');
+$fbcookie = get_facebook_cookie('115538591892230', '25302a5a33d08ecd3f3a21a9f738a12a');
+
+
+$config = array(
+    'appId' => '115538591892230',
+    'secret' => '25302a5a33d08ecd3f3a21a9f738a12a',
+    'cookie' => true,
+);
+
+$facebook = new Facebook($config);
+$facebook->setAccessToken($fbcookie['access_token']);
+$user_id = $facebook->getUser();
 
 
 ?>
-TEST
---
+++ php/test/login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>My CSS Test</title>
+<link href="css/login.css" rel="stylesheet" type="text/css" />
+<link href='http://fonts.googleapis.com/css?family=Ubuntu|Rosario' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+
+<script type="text/javascript">
+function fblogin(){
+	FB.api('/me', function(response){
+	$("#fbid").attr("value",response.id);
+	document.forms["loginform"].submit();
+	});
+
+}
+</script>
+
+</head>
+
+<body>
+
+
+   <div id="fb-root">
+       <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            appId      : '115538591892230',
+            status     : true, 
+            cookie     : true,
+            xfbml      : true
+          });
+        };
+        (function(d){
+           var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+           js = d.createElement('script'); js.id = id; js.async = true;
+           js.src = "//connect.facebook.net/en_US/all.js";
+           d.getElementsByTagName('head')[0].appendChild(js);
+         }(document));
+      </script>
+</div>
+<div class="fb-login-button" onlogin="fblogin();" perms="email">Login with Facebook</div>
+ 
+ 
+ <a href="#" onclick="FB.logout();">Logout of Facebook</a>
+ 
+
+
+
+
+
+</body>
+</html>
