0c280537328013e31014a5b3aab38512475bc852
Shaanan Cohney <shaananc@gmail.com>
2013-01-18 04:46:17 -0500

    trying to print

--- php/test/index.php
+++ php/test/index.php
@@ -2,8 +2,8 @@
 require_once('fb/facebook.php');
 
 $config = array(
-    'appId' => '115538591892230',
-    'secret' => '25302a5a33d08ecd3f3a21a9f738a12a',
+    'appId' => '413005548778403',
+    'secret' => '8c6d6dca17efdf37635a17e9157bac4c',
     'cookie' => true,
 );
 
@@ -11,5 +11,4 @@ $facebook = new Facebook($config);
 $user_id = $facebook->getUser();
 echo $user_id;
 echo "hello"
-
 ?>
--- php/test/login.php
+++ php/test/login.php
@@ -2,8 +2,8 @@
 require_once('fb/facebook.php');
 
 $config = array(
-    'appId' => '115538591892230',
-    'secret' => '25302a5a33d08ecd3f3a21a9f738a12a',
+    'appId' => '413005548778403',
+    'secret' => '8c6d6dca17efdf37635a17e9157bac4c',
     'cookie' => true,
 );
 
