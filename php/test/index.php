e130fc241797e1873910e734b814b73e1cb5d565
Shaanan Cohney <shaananc@gmail.com>
2013-01-18 04:59:15 -0500

    trying to print

--- php/test/index.php
+++ php/test/index.php
@@ -15,40 +15,14 @@ echo $user_id;
 ?>
   <?
     if($user_id) {
+        $data = array(
+                        'access_token' => $access_token,
+            'message' => 'Hi, This is my first message from facebook graph API. Downloaded from http://www.digimantra.com'
+        );
 
-      // We have a user ID, so probably a logged in user.
-      // If not, we'll get an exception, which we handle below.
-      try {
-        $ret_obj = $facebook->api('/me/feed', 'POST',
-                                    array(
-                                      'link' => 'www.example.com',
-                                      'message' => 'Posting with the PHP SDK!'
-                                 ));
-        echo '<pre>Post ID: ' . $ret_obj['id'] . '</pre>';
-
-      } catch(FacebookApiException $e) {
-        // If the user is logged out, you can have a 
-        // user ID even though the access token is invalid.
-        // In this case, we'll get an exception, so we'll
-        // just ask the user to login again here.
-        $login_url = $facebook->getLoginUrl( array(
-                       'scope' => 'publish_stream'
-                       )); 
-        echo 'Please <a href="' . $login_url . '">login.</a>';
-        error_log($e->getType());
-        error_log($e->getMessage());
-      }   
-      // Give the user a logout link 
-      echo '<br /><a href="' . $facebook->getLogoutUrl() . '">logout</a>';
-    } else {
-
-      // No user, so print a link for the user to login
-      // To post to a user's wall, we need publish_stream permission
-      // We'll use the current URL as the redirect_uri, so we don't
-      // need to specify it here.
-      $login_url = $facebook->getLoginUrl( array( 'scope' => 'publish_stream' ) );
-      echo 'Please <a href="' . $login_url . '">login.</a>';
-
+       
+      $return = json_decode(make_post('https://graph.facebook.com/' . $user->id . '/feed', $data));
+      
     } 
     echo 'success';
   ?> 
