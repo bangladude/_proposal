d22be02f805f5087480092d42560400ff8db951b
Shaanan Cohney <shaananc@gmail.com>
2013-01-18 03:17:18 -0500

    ie test

--- php/test/index.php
+++ php/test/index.php
@@ -4,17 +4,20 @@ echo "hello";
 require_once('__init__.php');
 // This would be the url of the host running the server-standalone.jar
 $wd_host = 'http://ftp.shaanan.cohney.info:4444/wd/hub'; // this is the default
-$web_driver = new WebDriver($wd_host);
+echo "hello";
+$web_driver = new WebDriver();
+
 
 // First param to session() is the 'browserName' (default = 'firefox')
 // Second param is a JSON object of additional 'desiredCapabilities'
 
 // POST /session
+echo "hello";
 $session = $web_driver->session('internet explorer');
 $session->open('http://www.facebook.com/cocacola');
 echo $session->source();
 
-echo "hello";
+
 
 ?>
 TEST
