10ff110238c8ea8664d43946a265098a92d5afb3
rigeljs <rigeljs@gmail.com>
2013-01-19 13:50:35 -0500

    add mapembed file

---
+++ php/mapembed.php
@@ -0,0 +1,26 @@
+<html>
+<?php
+
+function findlatlng($location){
+	$location = urlencode($location);
+	$url = 'https://maps.googleapis.com/maps/api/place/textsearch/xml?query=' . $location . '%20convention%20center&sensor=false&key=AIzaSyCDhpo3U3LZFhvXbte1-fiCEHaIWOY-lP4';
+	$xml = simplexml_load_file($url);
+  	$lat = $xml ->result->geometry->location->children();   
+	$latlng = $lat[0].",".$lat[1];
+	return $latlng;
+}
+
+function createurl(){
+	$coor = findlatlng();
+	$url = "http://maps.googleapis.com/maps/api/staticmap?size=512x512&maptype=roadmap\&markers=size:mid%7Ccolor:red%7C" . $coor . "&sensor=false";
+	return $url;
+}
+?>
+
+<img width="300" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="<?php echo createurl(); ?>" />
+<br />
+<small>
+<a href="<?php echo createurl(); ?>" style="color:#0000FF;text-align:left">View Larger Map</a>
+</small>
+
+</html>
