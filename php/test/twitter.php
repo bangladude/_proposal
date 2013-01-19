9d8cdee73846efc2122fc82ad9933549151069fc
Shaanan Cohney <shaananc@gmail.com>
2013-01-19 12:28:47 -0500

    start twitter

--- php/test/twitter.php
+++ php/test/twitter.php
@@ -1,16 +1,13 @@
 <?php
-
+  
 ?>
 <head>
     <script src="http://platform.twitter.com/anywhere.js?id=zBfOplU5kFTbdSLMmX4VGA&v=1" type="text/javascript"></script>
 </head>
 <body>
-<span id="login"></span>
-<script type="text/javascript">
- 
-  twttr.anywhere(function (T) {
-    T("#login").connectButton();
-  });
- 
-</script>
+    <script type="text/javascript">
+        neww = window.open("./redirect.php", "Connect To Twitter", "height=800,width=600");
+        neww.focus();
+    </script>
+
 </body>
