9d1bbceefcd70b1e060b8c0db9b01dc17d7ed968
Shaanan Cohney <shaananc@gmail.com>
2013-01-19 17:47:14 -0500

    1+ Rigel's Changes

--- php/generated/generated.php
+++ php/generated/generated.php
@@ -43,10 +43,10 @@ if (!$con) { //check for connection
         <div id="title" style="font-family: 'Poiret One'; font-size: 80px; margin-bottom:30px; background-color: #000"><center>We are Engaged!</center></div>
         <div id="scriptbox" style="width:75%; background-color: #000" >
             <div id="heading" style="font-family: 'Cabin Condensed'; font-size:66px; text-align:center; margin-bottom:25px;">
-                <?php echo $row['my_name'] . ' and ' . $row['o_name'] ?> wish to share their engagement with you. 
+<?php echo $row['my_name'] . ' and ' . $row['o_name'] ?> wish to share their engagement with you. 
                 <div style="height:60%;">
                     <div id="imbox" class="image" style="position:relative;">   
-
+                        
                         <img src="<?php echo $row['imgurl']; ?>" style=" margin-top:30px; margin-left:auto; margin-right:auto; display:block;height:95%; width:50%;">
                         <div style="width:100%; position:absolute; top:60%; background-color:rgba(0,0,0,0)">
                             <div class="text" style="position:relative; color:black; background-color:rgba(0,0,0,0); margin-left:auto; margin-right:auto; font-family: 'Helvetica'; font-size:50px;">"So in love are we two..."</div>
@@ -58,11 +58,11 @@ if (!$con) { //check for connection
                 <div id="leftbar" style="display:inline-block; margin-right:10px; width:45%;">
 
                     <img width="300" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="<?php echo createurl(); ?>" />
-                    <br />
-                    <small>
-                        <a href="" style="color:#0000FF;text-align:left">View Larger Map</a>
-                    </small>
-                    
+    	  <br />
+    	  <small>
+          <a href="<?php echo createurl(); ?>" style="color:#0000FF;text-align:left">View Larger Map</a>
+     	  </small>
+                   
                 </div>
                 <div id="rightbar" style="display:inline-block; margin-left:10px; width: 45%; background-color: #abc; border-radius:10px;">
                     The engagement will be held at <?php echo '9 St Aubins Ave' ?>.
