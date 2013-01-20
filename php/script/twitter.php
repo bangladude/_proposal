8b97364757897f0ee758f2114900a7addc496589
Shaanan Cohney <shaananc@gmail.com>
2013-01-20 04:35:59 -0500

    postTweet hotfix

--- php/script/twitter.php
+++ php/script/twitter.php
@@ -4,7 +4,7 @@ session_start();
 require_once('twitter/twitteroauth.php');
 require_once('config.php');
 
-function sendTweet(){
+function postTweet(){
 /* If access tokens are not available redirect to connect page. */
 if (empty($_SESSION['access_token']) || empty($_SESSION['access_token']['oauth_token']) || empty($_SESSION['access_token']['oauth_token_secret'])) {
     header('Location: ./clearsessions.php');
