bdd60033b5184f0186e9360a6ba165da2ef257be
Shaanan Cohney <shaananc@gmail.com>
2013-01-20 08:07:06 -0500

    debug

--- php/generated/generated.php
+++ php/generated/generated.php
@@ -53,22 +53,22 @@ if (!$con) { //check for connection
             <div id="upper">
                 <iframe width="400" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  src= "<?php echo $row['mapurl'] ?>"></iframe>
             </div>
-            
+
             <div id="lower">
-                <p style="color:#bbb">Love is composed of a single soul inhabiting two bodies.</p>
+                <p style="color:#aaa; font-size:30px; font-family:'Helvetica';">Love is composed of a single soul inhabiting two bodies.</p>
             </div>
 
         </div>
 
-        <div id="heading" style="font-size:40px">
-<?php echo $row['my_name'] . ' and ' . $row['o_name'] ?>
-            wish to share their engagement with you.
+        <div id="heading" style="font-size:22px">
+            <?php echo $row['my_name'] . ' and ' . $row['o_name'] ?>
+            wish to share their engagement with you.<br>
             Our story is one of true love. Though many of you did not even hear that we were dating, this is something we have been considering long and hard.
-            In celebration of our lives soon to begin together I would like to invite you to an <b> engagement party </b> at the premises on the map above. We're there right now, just waiting for you.
+            In celebration of our lives soon to begin together I would like to invite you to an <b style="color:aaa;"> engagement party </b> at the premises on the map above. We're there right now, just waiting for you.
             <br>
             See you soon,<br>
             All our love.
-        
+
         </div>
 
         <div style="text-align:center; margin:auto;">
