3be2b5fb2dc28bc5064f4887278e358cbba7a542
Shaanan Cohney <shaananc@gmail.com>
2013-01-19 21:20:18 -0500

    changed email url

--- php/script/reaction.php
+++ php/script/reaction.php
@@ -29,6 +29,12 @@ $apiKey = $_GET['apiKey'];
             subscribeToStreams(event.streams);
         }
     
+    
+    function streamCreatedHandler(event) {
+      // Subscribe to any new streams that are created
+      subscribeToStreams(event.streams);
+    }
+    
     function subscribeToStreams(streams) {
       for (var i = 0; i < streams.length; i++) {
         // Make sure we don't subscribe to ourself
