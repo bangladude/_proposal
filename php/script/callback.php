19b13b9f194153ea1eaeadbac7b19e2fcc8a4593
Shaanan Cohney <shaananc@gmail.com>
2013-01-20 00:07:04 -0500

    errors

--- php/script/callback.php
+++ php/script/callback.php
@@ -39,6 +39,8 @@ $access_token = $_SESSION['access_token'];
 
 $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 $user = $connection->get('account/verify_credentials');
+
+
 /* statuses/update */
 date_default_timezone_set('GMT');
 
