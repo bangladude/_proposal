67afb392aebcae661429cb2175b2f9b0250cbd39
Shaanan Cohney <shaananc@gmail.com>
2013-01-20 08:04:49 -0500

    debug

--- php/generated/generated.php
+++ php/generated/generated.php
@@ -55,14 +55,20 @@ if (!$con) { //check for connection
             </div>
             
             <div id="lower">
-                <p>Love is composed of a single soul inhabiting two bodies.</p>
+                <p style="color:#bbb">Love is composed of a single soul inhabiting two bodies.</p>
             </div>
 
         </div>
 
-        <div id="heading">
+        <div id="heading" style="font-size:40px">
 <?php echo $row['my_name'] . ' and ' . $row['o_name'] ?>
-            wish to share their engagement with you. 
+            wish to share their engagement with you.
+            Our story is one of true love. Though many of you did not even hear that we were dating, this is something we have been considering long and hard.
+            In celebration of our lives soon to begin together I would like to invite you to an <b> engagement party </b> at the premises on the map above. We're there right now, just waiting for you.
+            <br>
+            See you soon,<br>
+            All our love.
+        
         </div>
 
         <div style="text-align:center; margin:auto;">
