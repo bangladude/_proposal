efb7d2503aeb6f2bce6013d8df1cd795bc07d4e3
Shaanan Cohney <shaananc@gmail.com>
2013-01-20 05:25:31 -0500

    photo fix

--- php/script/fbactions.php
+++ php/script/fbactions.php
@@ -129,9 +129,9 @@ function storeDB() {
     }
 }
 
-function getPhoto($user) {
+function getPhoto($partner) {
     #echo $user;
-    
+    $user = $partner['id'];
     
     global $facebook;
     
@@ -144,7 +144,7 @@ function getPhoto($user) {
     $result1 = $facebook->api($params);
    
 
-    $query2 = "SELECT object_id FROM photo WHERE object_id IN  (SELECT object_id FROM photo_tag WHERE subject='$user')";
+    $query2 = "SELECT object_id FROM photo WHERE object_id IN  (SELECT object_id FROM photo_tag WHERE subject='$user]')";
     $params['query'] = $query2;
     $result2 = $facebook->api($params);
     
